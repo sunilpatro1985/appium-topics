afcb842822e875edf434ac989f3a63cfaf861b48
sunilpatro1985 <sunilpatro1985@gmail.com>
2023-08-03 20:34:54 +0530

    automate android chrome browser

---
+++ src/test/java/topics/web/AndroidChromeBr.java
@@ -0,0 +1,46 @@
+package topics.web;
+
+import base.AppDriver;
+import base.AppFactory;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+
+public class AndroidChromeBr {
+
+    @Test
+    public static void BrTestChrome() throws MalformedURLException, InterruptedException {
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName("emulator-5554")
+                .setPlatformVersion("13.0")
+                //.withBrowserName("chrome");
+                .setAppPackage("com.android.chrome")
+                .setAppActivity("com.google.android.apps.chrome.Main")
+                        .autoGrantPermissions();
+                AppFactory.android_launchApp(options);
+        Thread.sleep(1000);
+        System.out.println(((AndroidDriver)AppDriver.getDriver()).getContext());
+
+        ((AndroidDriver)AppDriver.getDriver()).context("WEBVIEW_chrome");
+        System.out.println(((AndroidDriver)AppDriver.getDriver()).getContext());
+        Thread.sleep(1000);
+
+
+        AppDriver.getDriver().get("https://qavbox.github.io/demo/signup/");
+        AppDriver.getDriver().findElement(By.id("email")).sendKeys("qavbox@gmail.com");
+
+        Thread.sleep(2000);
+        AppDriver.getDriver().quit();
+
+        //appium --allow-insecure chromedriver_autodownload
+        //https://github.com/appium/appium/issues/17492
+        //android 13 with chrome < 110 will be having some issue
+
+
+    }
+}
